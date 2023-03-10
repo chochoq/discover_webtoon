02a5ceeb6939d45e9b618ccb9ab94cc37903202e
chochoq <aubepe@gmail.com>
2023-03-10 13:25:16 +0800

    add Ui main

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,104 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.black,
-            body: Column(
-              children: [
-                SizedBox(height: 80),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'datadsds',
-                      style: TextStyle(
-                        color: Colors.white,
+            backgroundColor: Color(0xFF181818),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 80),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'menu',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Hey, NoName',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          Text(
+                            'Welcome back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 100),
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 22,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          'datadsds',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 38,
-                            fontWeight: FontWeight.w600,
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    '\$3 233 123',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 50,
+                          ),
+                          child: Text(
+                            'Transfer',
+                            style: TextStyle(
+                              fontSize: 22,
+                            ),
                           ),
                         ),
-                        Text(
-                          'dataa asdas',
-                          style: TextStyle(color: Colors.white),
+                        decoration: BoxDecoration(
+                            color: Colors.amber, borderRadius: BorderRadius.circular(45)),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 50,
+                          ),
+                          child: Text(
+                            'Request',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(79, 87, 85, 85),
+                            borderRadius: BorderRadius.circular(45)),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             )));
   }
 }
