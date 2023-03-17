579e993aff3dc31c4d353dba82675646b4aa6312
chochoq <aubepe@gmail.com>
2023-03-17 17:23:04 +0800

    add api fetching

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,140 +1,24 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-// 리스타트 만들기 리셋아이콘/ 재시작
-
-class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
-  late Timer timer;
-  bool isRunning = false;
-  int totalPomodoros = 0;
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-        totalPomodoros++;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds = totalSeconds - 1;
-      });
-    }
-  }
-
-  void onResetPressed() {
-    setState(() {
-      totalSeconds = twentyFiveMinutes;
-      isRunning = false;
-    });
-    timer.cancel();
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick,
-    );
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-
-    return duration.toString().split(".").first.substring(2, 7);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Flexible(
-              flex: 2,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  format(totalSeconds),
-                  style: TextStyle(
-                    color: Theme.of(context).cardColor,
-                    fontSize: 89,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              )),
-          Flexible(
-              flex: 2,
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      color: Theme.of(context).cardColor,
-                      onPressed: onResetPressed,
-                      icon: const Icon(Icons.settings_backup_restore_outlined),
-                      iconSize: 85,
-                    ),
-                    IconButton(
-                      color: Theme.of(context).cardColor,
-                      onPressed: isRunning ? onPausePressed : onStartPressed,
-                      icon: Icon(isRunning
-                          ? Icons.pause_circle_filled_outlined
-                          : Icons.play_circle_outline),
-                      iconSize: 80,
-                    ),
-                  ],
-                ),
-              )),
-          Flexible(
-              flex: 1,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).cardColor,
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'POMODOROS',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Theme.of(context).textTheme.headlineSmall!.color,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    Text(
-                      '$totalPomodoros',
-                      style: TextStyle(
-                          fontSize: 58,
-                          color: Theme.of(context).textTheme.headlineSmall!.color,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-              )),
-        ],
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          '오늘의 웹툰',
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Nanum Gothic',
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        elevation: 0,
       ),
     );
   }
