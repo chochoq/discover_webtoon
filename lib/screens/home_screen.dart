313307ac853c2d2068239602f465f49afab711b0
chochoq <aubepe@gmail.com>
2023-04-18 01:20:43 +0900

    add move screen widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:discover_webtoon/models/webtoon_model.dart';
 import 'package:discover_webtoon/services/api_service.dart';
+import 'package:discover_webtoon/widgets/webtoon_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -46,46 +47,15 @@ class HomeScreen extends StatelessWidget {
       itemCount: snapshot.data!.length,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        var webtoons = snapshot.data![index];
+        var webtoon = snapshot.data![index];
 
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ],
-              ),
-              child: Image.network(
-                webtoons.thumb,
-                // scale: 2.5,
-                headers: const {
-                  "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                },
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoons.title,
-              style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ],
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
         );
       },
-      separatorBuilder: (context, index) => const SizedBox(width: 40),
+      separatorBuilder: (context, index) => const SizedBox(width: 20),
     );
   }
 }
