740df957fc5fc334e140fc6bf0ee0721e2aa9cd0
chochoq <aubepe@gmail.com>
2023-04-17 20:25:02 +0900

    add webtoon image

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -29,19 +28,64 @@ class HomeScreen extends StatelessWidget {
           future: webtoons,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return ListView.separated(
-                itemCount: snapshot.data!.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  print(index);
-                  var webtoons = snapshot.data![index];
-                  return Text(webtoons.title);
-                },
-                separatorBuilder: (context, index) => const SizedBox(width: 20),
+              return Column(
+                children: [
+                  const SizedBox(height: 50),
+                  Expanded(child: makeList(snapshot)),
+                ],
               );
             }
             return const Center(child: CircularProgressIndicator());
           },
         ));
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemCount: snapshot.data!.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        var webtoons = snapshot.data![index];
+
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                webtoons.thumb,
+                // scale: 2.5,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoons.title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
+    );
   }
 }
