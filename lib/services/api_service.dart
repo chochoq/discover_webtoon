06287364c27ffbf82621f18394aa7a8a64482b3f
chochoq <aubepe@gmail.com>
2023-04-18 21:11:52 +0900

    add detail

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:discover_webtoon/models/webtoon_detail_model.dart';
+import 'package:discover_webtoon/models/webtoon_episode_model.dart';
 import 'package:discover_webtoon/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -20,6 +22,33 @@ class ApiService {
       }
 
       return webtoonIstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJSON(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getToonEpisodeById(String id) async {
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+
+    List<WebtoonEpisodeModel> episodesInstance = [];
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstance.add(WebtoonEpisodeModel.fromJson(episodes));
+      }
+      return episodesInstance;
     }
     throw Error();
   }
