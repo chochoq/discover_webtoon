3a0a3534e6bd06b6eca4a44ccb5e086595c32347
chochoq <aubepe@gmail.com>
2023-04-19 20:50:38 +0900

    add detail

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,8 +1,27 @@
+import 'package:discover_webtoon/models/webtoon_detail_model.dart';
+import 'package:discover_webtoon/models/webtoon_episode_model.dart';
+import 'package:discover_webtoon/services/api_service.dart';
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
   const DetailScreen({super.key, required this.title, required this.thumb, required this.id});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episode;
+
+  @override
+  void initState() {
+    super.initState();
+
+    webtoon = ApiService.getToonById(widget.id);
+    episode = ApiService.getToonEpisodeById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +30,7 @@ class DetailScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 30,
             fontFamily: 'Nanum Gothic',
@@ -21,30 +40,71 @@ class DetailScreen extends StatelessWidget {
         foregroundColor: Colors.green,
         elevation: 0,
       ),
-      body: Hero(
-        tag: id,
-        child: Container(
-          // width: 250,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15,
-                offset: const Offset(10, 10),
-                color: Colors.black.withOpacity(0.5),
+      body: Column(
+        children: [
+          Hero(
+            tag: widget.id,
+            child: Container(
+              // width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
               ),
-            ],
+              child: Image.network(
+                widget.thumb,
+                // scale: 2.5,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
           ),
-          child: Image.network(
-            thumb,
-            // scale: 2.5,
-            headers: const {
-              "User-Agent":
-                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-            },
-          ),
-        ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '작품소개',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return const Text("없ㅇㅁ");
+                  }
+                }),
+          )
+        ],
       ),
     );
   }
