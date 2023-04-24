cb785f34fc0584199aa4c545051ab4dfa13dc4fe
chochoq <aubepe@gmail.com>
2023-04-25 01:31:47 +0900

    fix img size

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -18,20 +18,18 @@ class ListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     String service = txt;
 
-    late String serviceName = '';
+    late String serviceEng = '';
     late Future webtoonApi;
-    if (service == 'naver' || service == '네이버 웹툰') {
+
+    if (service == '네이버 웹툰') {
       webtoonApi = webtoonsNaver;
-      serviceName = 'naver';
-      service == '네이버 웹툰';
-    } else if (service == 'kakaoPage') {
+      serviceEng = 'naver';
+    } else if (service == '카카페 웹툰') {
       webtoonApi = webtoonsKakaoPage;
-      service = '카카페 웹툰';
-      serviceName == 'kakaoPage';
+      serviceEng = 'kakaoPage';
     } else {
       webtoonApi = webtoonsKakao;
-      service = '카카오 웹툰';
-      serviceName == 'kakao';
+      serviceEng = 'kakao';
     }
 
     {
@@ -40,7 +38,7 @@ class ListScreen extends StatelessWidget {
           iconTheme: const IconThemeData(color: Colors.black),
           backgroundColor: Colors.transparent,
           title: Text(
-            "오늘의 $service",
+            "오늘의 $service ",
             style: const TextStyle(
               color: Colors.black,
               fontSize: 20,
@@ -60,87 +58,7 @@ class ListScreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       var webtoon = snapshot.data![index];
 
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => DetailScreen(
-                                      webtoonId: webtoon.id,
-                                      img: webtoon.thumb,
-                                      title: webtoon.title,
-                                      service: serviceName)));
-                        },
-                        child: Hero(
-                          tag: webtoon.id,
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 150,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: const Color(0xfffcf0f0)),
-                                padding: const EdgeInsets.only(left: 10, right: 6),
-                                child: Image.network(
-                                  webtoon.thumb,
-                                  height: 300,
-                                  scale: 0.5,
-                                  headers: const {
-                                    "User-Agent":
-                                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                                  },
-                                ),
-                              ),
-                              const SizedBox(width: 20),
-                              SizedBox(
-                                height: 64,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        SizedBox(
-                                          width: 200,
-                                          child: Text(
-                                            webtoon.title,
-                                            style: const TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 20,
-                                                fontFamily: 'Pretendard',
-                                                fontWeight: FontWeight.w700,
-                                                overflow: TextOverflow.ellipsis),
-                                          ),
-                                        ),
-                                        const SizedBox(width: 12),
-                                        const Icon(Icons.favorite_border_outlined, size: 20),
-                                      ],
-                                    ),
-                                    if (serviceName != 'naver')
-                                      Column(
-                                        children: const [
-                                          SizedBox(height: 6),
-                                          Opacity(
-                                            opacity: 0.50,
-                                            child: Text(
-                                              'webtoon',
-                                              style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 15,
-                                                fontFamily: "Pretendard",
-                                                fontWeight: FontWeight.w500,
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
+                      return webtoonBox(context, webtoon, serviceEng);
                     },
                     separatorBuilder: (context, index) => const SizedBox(height: 15),
                   );
@@ -150,5 +68,80 @@ class ListScreen extends StatelessWidget {
         ),
       );
     }
+  }
+
+  GestureDetector webtoonBox(BuildContext context, webtoon, String serviceEng) {
+    return GestureDetector(
+      onTap: () {
+        serviceEng == 'naver'
+            ? Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                        webtoonId: webtoon.id,
+                        img: webtoon.thumb,
+                        title: webtoon.title,
+                        service: serviceEng)))
+            : Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                        webtoonId: webtoon.id,
+                        img: webtoon.img,
+                        title: webtoon.title,
+                        service: serviceEng)));
+      },
+      child: Hero(
+        tag: webtoon.id,
+        child: Row(
+          children: [
+            Container(
+              height: 200,
+              width: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: const Color(0xfffcf0f0)),
+              padding: const EdgeInsets.only(left: 10, right: 6),
+              child: Image.network(
+                serviceEng == 'naver' ? webtoon.thumb : webtoon.img,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(width: 20),
+            SizedBox(
+              height: 64,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                          webtoon.title,
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w700,
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      // const Icon(Icons.favorite_border_outlined, size: 20),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
